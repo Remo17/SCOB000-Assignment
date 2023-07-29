fbbf6ab2048705f78c8a767db5f78ab3bdfc7b82
Remoratile <68880848+Remo17@users.noreply.github.com>
2023-07-29 14:00:38 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   int reg_modules,mark;
+cout<<"How many modules are you registered for?";
+cin>> reg_modules
+ 
+    
+    cin >> modules;
+for (int modules=0 ; modules<10; ++icount)
+    {
+
+    }//end of for
+    string  Name_Modules[modules];
+
+    return 0;
+}
